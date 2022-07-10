0c4c64c5df835cf2f2d172ff4d5c6b141e7a5e82
Eddddddddy <854214642@qq.com>
2022-07-10 18:31:31 +0100

    Update task_tools.py

--- lib/task/task_tools.py
+++ lib/task/task_tools.py
@@ -77,7 +77,10 @@ def movenetDecode(data, kps_mask=None, mode='output', num_joints=17,
                   img_size=192, hm_th=0.1):
     ##data [64, 7, 48, 48] [64, 1, 48, 48] [64, 14, 48, 48] [64, 14, 48, 48]
     # kps_mask [n, 7]
-    data = tf.transpose(data, [0, 3, 1, 2])
+    data[0] = tf.transpose(data[0], [0, 3, 1, 2])
+    data[1] = tf.transpose(data[1], [0, 3, 1, 2])
+    data[2] = tf.transpose(data[2], [0, 3, 1, 2])
+    data[3] = tf.transpose(data[3], [0, 3, 1, 2])
 
     if mode == 'output':
         # print(data[0])
